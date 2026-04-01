e0a18073e91875d194b153c05bd490a4ec5f9301
Kirill <ffa@bbb.ccc>
2026-04-01 13:31:55 +0700

    Добавлены сами треки

--- music/music/urls.py
+++ music/music/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
@@ -15,4 +18,4 @@ urlpatterns = [
     path ('add_track/',views.add_track),
     path ('edit_track/<int:id_track>',views.edit_track),
     path('artists/',views.artists),
-]
+]+static (settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
